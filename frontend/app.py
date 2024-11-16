80c9a7ec9c5d7fe6adf59a05bc4eb585e8e23b44
Laila <93746327+lailacodes@users.noreply.github.com>
2024-11-16 04:45:25 +0000

    On branch frontend Changes to be committed: 	modified:   frontend/app.py 	modified:   frontend/templates/index.html

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', current_date=datetime.now().strftime("%Y-%m-%d"))
+    return render_template('index.html', current_date=datetime.now(timezone(timedelta(hours=-5), 'EST')).strftime("%H:%M:%S"))
 
 if __name__ == '__main__':
     app.run(debug=True)
